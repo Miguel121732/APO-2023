d532c8a3a71a19cf004f92cf70281c85b58d8fe1
Miguel121732 <125085406+Miguel121732@users.noreply.github.com>
2023-02-10 16:51:00 -0500

    actualizacion

---
+++ Principal/src/Principal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//nombre de la clase
+public class Principal {
+  
+    //metodo principal (main)
+    public static void main(String[] args) {
+    	
+    	System.out.println(contarPalabras());
+	
+    }
+public static String contarPalabras(){
+	
+	
+	System.out.println("ingrese la palabra");
+	//clase que permite capturar datos por consola
+    Scanner sc = new Scanner (System.in);
+
+    
+    String prueba = sc.next();
+    
+    return "la cantidad de letras que tiene la palabra es :".concat(prueba.toUpperCase());
+   
+ }
+
+}
+
